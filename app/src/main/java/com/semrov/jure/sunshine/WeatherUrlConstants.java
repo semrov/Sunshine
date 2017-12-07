9d645cb21bae944eddf1155c97cac03505bbd312
Jure <jure.semrov@outlook.com>
2017-12-07 22:13:37 +0100

    Add a class containing constants for accessing weather data

---
+++ app/src/main/java/com/semrov/jure/sunshine/WeatherUrlConstants.java
@@ -0,0 +1,15 @@
+package com.semrov.jure.sunshine;
+
+/**
+ * Created by Jure on 7.12.2017.
+ */
+
+//Constants
+public final class WeatherUrlConstants
+{
+    public static final String APPID = "ccdb30e5915364ec2380ab284e3ada58";
+    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+    public static final String QUERY_PARAM = "q";
+    public static final String UNITS_PARAM = "units";
+
+}
