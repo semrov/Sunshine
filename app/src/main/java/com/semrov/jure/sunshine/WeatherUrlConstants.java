e276be00014e55ca784e9cff595be63a469a0e0d
Jure <jure.semrov@outlook.com>
2017-12-07 22:26:13 +0100

    Add new constants

--- app/src/main/java/com/semrov/jure/sunshine/WeatherUrlConstants.java
+++ app/src/main/java/com/semrov/jure/sunshine/WeatherUrlConstants.java
@@ -8,8 +8,10 @@ package com.semrov.jure.sunshine;
 public final class WeatherUrlConstants
 {
     public static final String APPID = "ccdb30e5915364ec2380ab284e3ada58";
+    public static final String APPID_PARAM = "appid";
     public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?";
     public static final String QUERY_PARAM = "q";
+    public static final String FORMAT_PARAM = "mode";
     public static final String UNITS_PARAM = "units";
 
 }
