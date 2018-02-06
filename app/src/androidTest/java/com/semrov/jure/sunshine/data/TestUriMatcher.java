8dc0a771bcc46feb703cd85c2581a87c878467ca
Jure <jure.semrov@outlook.com>
2018-02-06 18:14:38 +0100

    Add tests for WeatherProvider and Contract and UriMatcher

---
+++ app/src/androidTest/java/com/semrov/jure/sunshine/data/TestUriMatcher.java
@@ -0,0 +1,54 @@
+package com.semrov.jure.sunshine.data;
+
+/**
+ * Created by Jure on 29.1.2018.
+ */
+
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.semrov.jure.sunshine.data.WeatherContract;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class TestUriMatcher
+{
+    /*
+    private static final String LOCATION_QUERY = "London, UK";
+    private static final long TEST_DATE = 1419033600L;  // December 20th, 2014
+    private static final long TEST_LOCATION_ID = 10L;
+
+    // content://com.example.android.sunshine.app/weather"
+    private static final Uri TEST_WEATHER_DIR = WeatherContract.WeatherEntry.CONTENT_URI;
+    private static final Uri TEST_WEATHER_WITH_LOCATION_DIR = WeatherContract.WeatherEntry.buildWeatherLocation(LOCATION_QUERY);
+    private static final Uri TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR = WeatherContract.WeatherEntry.buildWeatherLocationWithDate(LOCATION_QUERY, TEST_DATE);
+    // content://com.example.android.sunshine.app/location"
+    private static final Uri TEST_LOCATION_DIR = WeatherContract.LocationEntry.CONTENT_URI;
+
+    */
+
+    /*
+        Students: This function tests that your UriMatcher returns the correct integer value
+        for each of the Uri types that our ContentProvider can handle.  Uncomment this when you are
+        ready to test your UriMatcher.
+     */
+//    public void testUriMatcher() {
+//        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
+//
+//        assertEquals("Error: The WEATHER URI was matched incorrectly.",
+//                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
+//        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
+//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR), WeatherProvider.WEATHER_WITH_LOCATION);
+//        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
+//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR), WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
+//        assertEquals("Error: The LOCATION URI was matched incorrectly.",
+//                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
+//    }
+}
---
+++ app/src/androidTest/java/com/semrov/jure/sunshine/data/TestWeatherContract.java
@@ -0,0 +1,39 @@
+package com.semrov.jure.sunshine.data;
+
+
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+
+/*
+    Students: This is NOT a complete test for the WeatherContract --- just for the functions
+    that we expect you to write.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class TestWeatherContract {
+    // intentionally includes a slash to make sure Uri is getting quoted correctly
+    private static final String TEST_WEATHER_LOCATION = "/North Pole";
+    private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+
+    public void testBuildWeatherLocation() {
+        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
+                        "WeatherContract.",
+                locationUri);
+        assertEquals("Error: Weather location not properly appended to the end of the Uri",
+                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+        assertEquals("Error: Weather location Uri doesn't match our expected result",
+                locationUri.toString(),
+                "content://com.example.android.sunshine.app/weather/%2FNorth%20Pole");
+    }
+}
