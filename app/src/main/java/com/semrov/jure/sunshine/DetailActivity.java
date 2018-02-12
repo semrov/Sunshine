ca07df8703afb9ad56d0a38de67e33106c71318e
Jure <jure.semrov@outlook.com>
2018-02-12 20:03:22 +0100

    DetailActivity now works again, including sharing data.

--- app/src/main/java/com/semrov/jure/sunshine/DetailActivity.java
+++ app/src/main/java/com/semrov/jure/sunshine/DetailActivity.java
@@ -1,7 +1,13 @@
 package com.semrov.jure.sunshine;
 
 import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +22,11 @@ import android.view.ViewGroup;
 import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
+import com.semrov.jure.sunshine.data.WeatherContract;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private static final int LOADER_ID = 0;
 
 
     @Override
@@ -55,11 +65,35 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
-    public static class DetailFragment extends Fragment
+    public static class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>
     {
         private static final String LOG_TAG = DetailFragment.class.getSimpleName();
         private static final String FORECAST_SHARE_TAG = "#SunshineApp";
         private String mforecastString;
+        private ShareActionProvider mShareActionProvider;
+
+        private static final String[] FORECAST_COLUMNS = {
+                // In this case the id needs to be fully qualified with a table name, since
+                // the content provider joins the location & weather tables in the background
+                // (both have an _id column)
+                // On the one hand, that's annoying.  On the other, you can search the weather table
+                // using the location set by the user, which is only in the Location table.
+                // So the convenience is worth it.
+                WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+                WeatherContract.WeatherEntry.COLUMN_DATE,
+                WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+        };
+
+
+        // these constants correspond to the projection defined above, and must change if the
+        // projection changes
+        private static final int COL_WEATHER_ID = 0;
+        private static final int COL_WEATHER_DATE = 1;
+        private static final int COL_WEATHER_DESC = 2;
+        private static final int COL_WEATHER_MAX_TEMP = 3;
+        private static final int COL_WEATHER_MIN_TEMP = 4;
 
         public DetailFragment()
         {
@@ -67,12 +101,25 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            // Prepare the loader.  Either re-connect with an existing one,
+            // or start a new one.
+            getLoaderManager().initLoader(LOADER_ID,null,this);
+        }
+
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail,container,false);
+            /*
             Intent i = getActivity().getIntent();
+            if (i != null)
+            {
+                mforecastString = i.getDataString();
+            }
             TextView tv = rootView.findViewById(R.id.tv_detail_forecast);
-            mforecastString = i.getStringExtra("forecast");
             tv.setText(mforecastString);
+            */
             return rootView;
         }
 
@@ -85,12 +132,12 @@ public class DetailActivity extends AppCompatActivity {
             MenuItem menuItem = menu.findItem(R.id.action_share);
 
             // Fetch and store ShareActionProvider
-            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
             // Attach an intent to this ShareActionProvider.  You can update this at any time,
             // like when the user selects a new piece of data they might like to share.
-            if (shareActionProvider != null) {
-                shareActionProvider.setShareIntent(createShareIntent());
+            if (mShareActionProvider != null && mforecastString != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
             }
 
         }
@@ -105,5 +152,38 @@ public class DetailActivity extends AppCompatActivity {
             return shareIntent;
         }
 
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            Intent i = getActivity().getIntent();
+
+            if (i != null)
+            {
+                Uri uri = i.getData();
+                return new CursorLoader(getActivity(),uri,FORECAST_COLUMNS,null,null,null);
+            }
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            if(!data.moveToFirst()) {return;}
+            String dateString = Utility.formatDate(data.getLong(COL_WEATHER_DATE));
+            String weatherDesc = data.getString(COL_WEATHER_DESC);
+            boolean isMetric = Utility.isMetric(getActivity());
+            String high = Utility.formatTemperature(data.getDouble(COL_WEATHER_MAX_TEMP),isMetric);
+            String low = Utility.formatTemperature(data.getDouble(COL_WEATHER_MIN_TEMP),isMetric);
+
+            mforecastString = String.format("%s - %s - %s/%s", dateString, weatherDesc, high, low);
+            TextView detailTv = getView().findViewById(R.id.tv_detail_forecast);
+            detailTv.setText(mforecastString);
+
+            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {}
     }
 }
--- app/src/main/java/com/semrov/jure/sunshine/ForecastFragment.java
+++ app/src/main/java/com/semrov/jure/sunshine/ForecastFragment.java
@@ -18,6 +18,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import com.semrov.jure.sunshine.data.WeatherContract;
 
@@ -89,17 +90,23 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-        /*
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String forecast = mForecastAdapter.getItem(i);
-                Intent intent = new Intent(getActivity(),DetailActivity.class);
-                intent.putExtra("forecast",forecast);
-                startActivity(intent);
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(i);
+                if (cursor != null)
+                {
+                    String locationSetting = Utility.getPreferredLocation(getActivity());
+                    Intent intent = new Intent(getActivity(),DetailActivity.class)
+                            .setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(locationSetting,
+                                    cursor.getLong(COL_WEATHER_DATE)));
+                    startActivity(intent);
+                }
+
             }
         });
-        */
+
         return rootView;
     }
 
