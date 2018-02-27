eb68a0a3e916dbbf83b7a86dc202a1481a08d7e8
Jure <jure.semrov@outlook.com>
2018-02-27 21:17:22 +0100

    Bugfix - in SettingsActivity - DetailFragment is blank after back button is clicked

--- app/src/main/java/com/semrov/jure/sunshine/SettingsActivity.java
+++ app/src/main/java/com/semrov/jure/sunshine/SettingsActivity.java
@@ -1,5 +1,8 @@
 package com.semrov.jure.sunshine;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,4 +19,11 @@ public class SettingsActivity extends AppCompatActivity {
                     .commit();
         }
     }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
+
 }
